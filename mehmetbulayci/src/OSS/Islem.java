6a720f1eb4a9d71ff4821ebc1df9414525d5e086
Mehmet Bulayci <stopserious9506@gmail.com>
2017-04-07 18:04:15 +0300

    Soru ödevi.Vizelerden dolayı kısa kestim

---
+++ mehmetbulayci/src/OSS/Islem.java
@@ -0,0 +1,27 @@
+package OSS;
+
+import Sorular.Soru1;
+
+public class Islem {
+	public static void main(String[] args) {
+		Soru1 soru1 = new Soru1();
+		System.out.println("Soru 1:");
+		soru1.soru1();
+	    System.out.println(" ");
+		System.out.println("Soru 2:");
+	    soru1.soru2();
+		
+		System.out.println(" ");
+		System.out.println("Soru 3:");
+	    soru1.soru3();
+	  
+	    System.out.println(" ");
+	    System.out.println("Soru 4:");
+	    soru1.soru4();
+	    
+
+
+	}
+
+	}
+
